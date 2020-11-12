8e7710a7bccce93f6fc0eee8d48da8a180641db8
starbuckshiker <u1304954@umail.utah.edu>
2020-11-12 16:46:42 -0700

    js update

---
+++ loops.js
@@ -0,0 +1,26 @@
+var officeItems = ["stapler", "monitor", "computer", "desk", "lamp", "computer", "lamp", "stapler", "computer", "computer"]
+
+var count = 0;
+for(var i = 0; i < array.length; ++i){
+    if(array[i] == computer)
+        count++;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("Mike is not old enough to see Mad Max")
