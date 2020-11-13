d8276cd31f21f57cb6081625b1a7be4a9f64d413
starbuckshiker <u1304954@umail.utah.edu>
2020-11-12 17:18:11 -0700

    counts

--- loops.js
+++ loops.js
@@ -1,12 +1,39 @@
 var officeItems = ["stapler", "monitor", "computer", "desk", "lamp", "computer", "lamp", "stapler", "computer", "computer"]
-
-var count = 0;
-for(var i = 0; i < array.length; ++i){
-    if(array[i] == computer)
-        count++;
+var count = (officeItems.match(/computer/g)) || []).length;
+console.log(count);
 
 
 
+        var peopleWhoWantToSeeMadMaxFuryRoad = [
+            {
+              name: "Mike",
+              age: 12,
+              gender: "male"
+            },{
+              name: "Madeline",
+              age: 80,
+              gender: "female"
+            },{
+              name: "Cheryl",
+              age: 22,
+              gender: "female"
+            },{
+              name: "Sam",
+              age: 30,
+              gender: "male"
+            },{
+              name: "Suzy",
+              age: 4,
+              gender: "female"
+            }
+          ] 
+          for(let x = 0; x < people.length; x++) {
+            if(people[x].age >= 18){
+               console.log(people[x].name + " Is Old Enough");
+            }else {
+               console.log(people[x].name + " Is Not Old Enough");
+            } 
+       }
 
 
 
@@ -24,3 +51,5 @@ for(var i = 0; i < array.length; ++i){
 
 
 console.log("Mike is not old enough to see Mad Max")
+console.log()
+        
